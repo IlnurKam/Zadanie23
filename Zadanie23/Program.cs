603ae1fe742435cbed773d16d48d15e6def7bf97
IlnurKam <kamal673@yandex.ru>
2022-03-22 18:52:23 +0300

    Добавьте файлы проекта.

---
+++ Zadanie23/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Homework23
+{
+    class Program
+    {
+        public static long FactorialAsync(int N)
+        {
+            long factotial = 1;
+            if (N != 0)
+            {
+                for (int i = 2; i <= N; i++)
+                {
+                    factotial *= i;
+                }
+            }
+            return factotial;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число N: ");
+            int N = Int32.Parse(Console.ReadLine());
+            if (N >= 0)
+            {
+                Console.WriteLine($"Факториал числа {N}: {FactorialAsync(N)}");
+            }
+            else
+            {
+                Console.WriteLine("Было введено отрицательное число!");
+            }
+            Console.Write("Нажмите любую клавишу для выхода из программы...");
+            Console.ReadKey();
+        }
+    }
+}
